c5cc1ad5e53c8d867071f4f6c5c15ce86993d9c4
Josh Schonstal <josh@schonstal.org>
2016-08-27 11:11:13 -0700

    💾 Add luxe boilterplate

---
+++ src/Main.hx
@@ -0,0 +1,26 @@
+
+import luxe.GameConfig;
+import luxe.Input;
+
+class Main extends luxe.Game {
+  override function config(config:GameConfig) {
+    config.window.title = 'Ancient Technology';
+    config.window.width = 960;
+    config.window.height = 640;
+    config.window.fullscreen = false;
+
+    return config;
+  }
+
+  override function ready() {
+  }
+
+  override function onkeyup( e:KeyEvent ) {
+    if(e.keycode == Key.escape) {
+      Luxe.shutdown();
+    }
+  }
+
+  override function update(dt:Float) {
+  }
+}
